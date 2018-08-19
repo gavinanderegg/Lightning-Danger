edb3a1f64a5d1196575eef0d5ddb1eb41e0d1e14
Gavin Anderegg <gavin@anderegg.ca>
2018-08-19 20:14:28 -0300

    Grabbing images

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import shutil
 
 # https://weather.gc.ca/lightning/index_e.html?id=NAT
 # https://weather.gc.ca/lightning/index_e.html?id=ATL
@@ -11,4 +12,14 @@ soup = BeautifulSoup(r.content, 'html.parser')
 num_images = soup.find(id='wxo-animator').attrs['data-image-count']
 
 for count in range(0, int(num_images)):
-    print(soup.find(id='wxo-animator').attrs['data-wxo-anim-' + str(count)])
+    current_image = soup.find(id='wxo-animator').attrs['data-wxo-anim-' + str(count)]
+
+    img = requests.get('https://weather.gc.ca' + current_image, stream=True)
+
+    filename = current_image.split('/')[-1:][0]
+
+    if img.status_code == 200:
+        with open('./img/' + filename, 'wb') as f:
+            img.raw.decode_content = True
+            shutil.copyfileobj(img.raw, f)
+
