c8fe5080506050656e45caff7529b5409469ecf1
Gavin Anderegg <gavin@anderegg.ca>
2018-08-19 19:54:07 -0300

    Get image URLs

--- main.py
+++ main.py
@@ -8,4 +8,7 @@ r = requests.get('https://weather.gc.ca/lightning/index_e.html?id=ATL')
 
 soup = BeautifulSoup(r.content, 'html.parser')
 
-print(soup.find(id='wxo-animator'))
+num_images = soup.find(id='wxo-animator').attrs['data-image-count']
+
+for count in range(0, int(num_images)):
+    print(soup.find(id='wxo-animator').attrs['data-wxo-anim-' + str(count)])
